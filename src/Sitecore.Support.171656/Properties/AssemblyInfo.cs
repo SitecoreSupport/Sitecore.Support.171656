e93166286dd206e0e2797415774e1042a243847f
Dmitry Kolinchuk <dmko@sitecore.net>
2017-07-06 13:46:16 +0300

    Update version of the jquery-ui.min.js script to 1.11.3 to prevent the error when using the Date Picker field in WFFM

---
+++ src/Sitecore.Support.171656/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.171656")]
+[assembly: AssemblyProduct("Sitecore.Support.171656")]
+[assembly: ComVisible(false)]
